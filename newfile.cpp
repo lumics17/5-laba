06023d6c7c9366afb2d63605cf3cdb47f2c7bde9
lumics17 <147649733+lumics17@users.noreply.github.com>
2023-11-17 23:32:07 +0300

    Update newfile.cpp

--- newfile.cpp
+++ newfile.cpp
@@ -4,29 +4,6 @@
 #include <math.h>
 using namespace std;
 
-double TPIntegral(double a,double b,double(*func)(double),int N){
-    double answer = 0,x1 = 0, x2 = 0;
-    double width=(b-a)/N;
-    for(int i=1;i<N+1;i++){
-      x1 = a + i*width;
-      x2 = a + (i+1)*width;
-        answer+=(x2-x1)*0.5*(func(x1)+func(x2));
-    }
-    return answer;
-}
-double SMIntegral(double a,double b,double(*func)(double),int N){
-    double answer=0,x1 = 0, x2 = 0;
-    double width=(b-a)/N;
-    for(int i=1;i<N+1;i++){
-      x1 = a + i*width;
-      x2 = a + (i+1)*width;
-        answer+= (x2-x1)/6.0*(func(x1) + 4.0*func(0.5*(x1+x2)) + func(x2));
-    }
-    return answer;
-}
-
-
-
 double xi(int i,double widthx,double a){
     double xi=a+i*widthx;
     return xi;
@@ -34,7 +11,43 @@ double xi(int i,double widthx,double a){
 
 double yj(int j,double widthy,double b){
     double yj=b+j*widthy;
-    return yj;
+    return yj;  
+}
+double TPIntegral(double a,double b,double(*func)(double),double eps){
+    int N=1;
+    double width=(b-a)/N;
+    double Integral=func(a) + func(b);
+    double Integral2;
+   while (abs(Integral-Integral2)>3*eps){
+        N*=2;
+        Integral2=Integral;
+        width=(b-a)/N;
+        for(int i=0;i<N-1;i++){
+            Integral+=2*func(a+i*width);
+        }
+        Integral*=width/2;
+    }
+    return Integral;
+}
+double SMIntegral(double a,double b,double(*func)(double),double eps){
+    
+     int N=1;
+    double width=(b-a)/N;
+    double Integral=func(a) + func(b);
+    double Integral2;
+   while (abs(Integral-Integral2)>15*eps){
+        N*=2;
+        Integral2=Integral;
+        width=(b-a)/N;
+        for(int i=1;i<N/2;i++){
+            Integral+=4*func(a+(2*i-1)*width);
+        }
+        for(int i=1;i<N/2-1;i++){
+            Integral+=2*func(a+(2*i)*width);   
+        }
+        Integral*=width/3;
+    }
+    return Integral;
 }
 double DISMIntegral(double a,double A,double b,double B,double(*func)(double,double),int N,int M){
     double answer=0;
@@ -46,7 +59,7 @@ double DISMIntegral(double a,double A,double b,double B,double(*func)(double,dou
 //            xi=a+i*widthx;
 //            yj=b+j*widthy;
 //           answer+=func(2*xi,2*yj) + 4*func(2*xi+1,2*yj) + func(2*xi+2,2*yj) + 4*func(2*xi,2*yj+1) + 16*func(2*xi+1,2*yj+1) + 4*func(2*xi+2,2*yj+1) + func(2*xi,2*yj+2) + 4*func(2*xi+1,2*yj+2) + func(2*xi+2,2*yj+2);
-             answer+=  func(xi(2*i,widthx,a),yj(2*j,widthy,b)) 
+             answer += func(xi(2*i,widthx,a),yj(2*j,widthy,b)) 
                      
                      + 4*func(xi(2*i+1,widthx,a),yj(2*j,widthy,b)) 
                      
@@ -77,6 +90,8 @@ double TaskDoubleIN(double x,double y){
     return sin(x+y);
 }
 void printAnswer(double answer){
-    cout<<"Answer = "<<answer<<endl;
+    cout<<setprecision(10)<<"Answer = "<<answer<<endl;
 }
+
+
 
